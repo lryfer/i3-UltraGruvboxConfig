980c5b465059d67fb14d3130fb6171fc7daf3b95
xanteraka <96692294+xanteraka@users.noreply.github.com>
2022-07-05 15:24:32 +0200

    Add files via upload

---
+++ polybar/launch.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+DIR="$HOME/.config/polybar"
+killall -q polybar
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+polybar -q main -c "$DIR"/config.ini &
+polybar -q main2 -c "$DIR"/config.ini &
