c309ce227904d6d45734f2e7acd2971e4d4cc7cb
xanteraka <96692294+xanteraka@users.noreply.github.com>
2022-09-20 09:44:54 +0200

    Add files via upload

---
+++ autoinstall.sh
@@ -0,0 +1,57 @@
+sudo echo ""
+echo "========================================================================="
+echo "setting the folders in the directory .config"
+if [[ $(ls ~/.config/ | grep "OldConfig") ]]; then
+  echo "it seems the scripts already run on the system, you might \n
+  need to remove the folder ~/.config/OldConfig folder to execute the script"
+  exit 
+fi
+mkdir ~/.config/OldConfig
+if [[ $(ls ~/.config/ | grep "alacritty") ]]; then
+  mv ~/.config/alacritty/ ~/.config/OldConfig
+
+fi
+cp -r src/alacritty/ ~/.config/
+
+if [[ $(ls ~/.config/ | grep "alacritty") ]]; then
+  mv ~/.config/alacritty/ ~/.config/OldConfig
+fi
+cp -r src/alacritty/ ~/.config/ 
+
+if [[ $(ls ~/.config/ | grep "i3") ]]; then
+  mv ~/.config/i3/ ~/.config/OldConfig
+fi
+cp -r src/i3/ ~/.config/
+
+if [[ $(ls ~/.config/ | grep "picom") ]]; then
+  mv ~/.config/picom/ ~/.config/OldConfig
+fi
+cp -r src/picom/ ~/.config/
+
+if [[ $(ls ~/.config/ | grep "fish") ]]; then
+  mv ~/.config/fish/ ~/.config/OldConfig
+fi
+cp -r src/fish/ ~/.config/
+
+if [[ $(ls ~/.config/ | grep "polybar") ]]; then
+  mv ~/.config/polybar/ ~/.config/OldConfig
+fi
+cp -r src/polybar/ ~/.config/
+sudo chmod 777 ~/.config/polybar/scripts/*
+echo "========================================================================="
+
+echo "getting nerd JetBrainsMono Nerd Fonts"
+curl -LO https://github.com/ryanoasis/nerd-fonts/releases/download/v2.2.2/JetBrainsMono.zip
+unzip JetBrainsMono.zip
+mkdir -p JBNF
+mv *.ttf JBNF && rm -r JetBrainsMono.zip
+sudo mv JBNF /usr/share/fonts/ 
+echo "JetBrainsMono Nerd Fonts installed in the directory /usr/share/fonts/JBNF"
+echo "========================================================================="
+echo "installing oh my fish framework for themes"
+curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
+omf install bobthefish
+echo "========================================================================="
+echo "                                 done                                    "
+echo "========================================================================="
+
---
+++ dependecycheck.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+$misdep 
+misdep=""
+if [[ -z $(command -v polybar) ]]; then
+  misdep+=" polybar" 
+fi
+if [[ -z $(command -v alacritty) ]]; then
+  misdep+=" alacritty"
+fi
+if [[ -z $(command -v i3) ]]; then
+  misdep+=" i3-gaps/i3-wm"
+fi
+if [[ -z $(command -v fish) ]]; then
+  misdep+=" fish"
+fi
+if [[ -z $(command -v picom) ]]; then
+  misdep+=" picom"
+fi
+if [[ -z $(command -v feh) ]]; then
+  misdep+=" feh"
+fi
+if [[ $misdep != "" ]]; then
+  echo ${misdep:1}" is/are missing in the system"
+  if [[ $(command -v pacman) ]]; then
+  sudo pacman -S alacritty fish i3-gaps picom polybar feh i3lock  
+else
+  echo "auto installer is not implemented for your system"
+fi
+else
+  echo 'everything is installed'
+fi
+  
