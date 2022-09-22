451741ace077bd4df483bca3235aa221e431961f
xanteraka <96692294+xanteraka@users.noreply.github.com>
2022-09-22 09:36:19 +0200

    Update launch.sh

--- src/polybar/launch.sh
+++ src/polybar/launch.sh
@@ -4,3 +4,10 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 polybar -q main -c "$DIR"/config.ini &
 polybar -q main2 -c "$DIR"/config.ini &
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload example &
+  done
+else
+  polybar --reload example &
+fi
