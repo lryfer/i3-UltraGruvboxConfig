fc61feac55f698e49591726bdfc470fffc61a906
Jonathan <contact@grafikart.fr>
2022-10-17 18:29:05 +0200

    fix and optimise missing dependencies

--- dependecycheck.sh
+++ dependecycheck.sh
@@ -8,7 +8,7 @@ if [[ -z $(command -v alacritty) ]]; then
   misdep+=" alacritty"
 fi
 if [[ -z $(command -v i3) ]]; then
-  misdep+=" i3-gaps/i3-wm"
+  misdep+=" i3-gaps"
 fi
 if [[ -z $(command -v fish) ]]; then
   misdep+=" fish"
@@ -19,15 +19,27 @@ fi
 if [[ -z $(command -v feh) ]]; then
   misdep+=" feh"
 fi
+if [[ -z $(command -v rofi) ]]; then
+  misdep+=" rofi"
+fi
+if [[ -z $(command -v htop) ]]; then
+  misdep+=" htop"
+fi
+if [[ -z $(command -v gdu) ]]; then
+  misdep+=" gdu"
+fi
+if [[ -z $(command -v i3lock) ]]; then
+  misdep+=" i3lock"
+fi
 if [[ $misdep != "" ]]; then
   echo ${misdep:1}" is/are missing in the system"
   if [[ $(command -v pacman) ]]; then
-  sudo pacman -S alacritty fish i3-gaps picom polybar feh i3lock
-elif [[ $(command -v dnf) ]]; then
-  sudo dnf install alacritty fish i3-gaps picom polybar feh i3lock
-else
-  echo "auto installer is not implemented for your system"
-fi
+    sudo pacman -S ${misdep}
+  elif [[ $(command -v dnf) ]]; then
+    sudo dnf install ${misdep}
+  else
+    echo "auto installer is not implemented for your system"
+  fi
 else
   echo 'everything is installed'
 fi
