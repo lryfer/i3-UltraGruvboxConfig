1f15116c5b47d079746a7e1e3d143d2bc743efc8
Gabriel Guillou <52610162+Pikatsuto@users.noreply.github.com>
2022-10-17 22:59:21 +0200

    fix multi screen polybar

--- src/polybar/launch.sh
+++ src/polybar/launch.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env bash
 DIR="$HOME/.config/polybar"
 killall -q polybar
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-polybar -q main -c "$DIR"/config.ini &
-polybar -q main2 -c "$DIR"/config.ini &
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload example &
+    MONITOR=$m polybar --reload main &
   done
 else
   polybar --reload example &
